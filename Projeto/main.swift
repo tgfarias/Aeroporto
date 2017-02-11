7f0830af6ddf043ed49803ed174e36c89b765119
Faculdade Catolica do Tocantins <Facto09@Mac-mini-de-Faculdade-60.local>
2017-02-11 10:08:05 -0200

    Initial Commit

---
+++ Projeto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projeto
+//
+//  Created by Faculdade Catolica do Tocantins on 11/02/17.
+//  Copyright © 2017 Faculdade Catolica do Tocantins. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
